318d853d4b3a43b3027e6e35b51612f0f6f25739
Salih Bicakci <salih.bicakci@morhipo.com>
2019-02-21 10:33:32 +0300

    Add project files.

---
+++ src/Payments/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using MassTransit;
+
+namespace Payments
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bus = Bus.Factory.CreateUsingRabbitMq(sbc =>
+            {
+                var host = sbc.Host(new Uri("rabbitmq://51.158.76.230"), h =>
+                {
+                    h.Username("admin");
+                    h.Password("admin");
+                });
+
+                sbc.ReceiveEndpoint(host, "Payments", ep =>
+                {
+                    ep.Consumer(() => new OrderRequestedConsumer());
+                });
+            });
+
+            bus.Start();
+            
+            Console.WriteLine("Welcome to Payments");
+            Console.WriteLine("Press Q key to exit");
+            while (Console.ReadKey(true).Key != ConsoleKey.Q) ;
+
+            bus.Stop();
+        }
+    }
+}
